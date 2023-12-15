7c29f5cad266d91e9b2667a3f32ecca76c04939a
tinsygit <tinsyding@gmail.com>
2023-12-16 01:34:48 +0800

    支持以目录树的样式展示文件夹中所有文件的MD5值

--- md5.py
+++ md5.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 
 def get_md5_for_file(filename):
     """计算并返回文件的MD5哈希值"""
@@ -15,12 +16,31 @@ def get_md5_for_string(s):
     """计算并返回字符串的MD5哈希值"""
     return hashlib.md5(s.encode()).hexdigest()
 
+def print_md5_for_directory(directory, prefix=''):
+    """以目录树样式打印文件夹内所有文件的MD5值"""
+    if not os.path.isdir(directory):
+        print("文件夹未找到或为空")
+        return
+
+    files = os.listdir(directory)
+    files.sort()  # 确保顺序一致
+    for i, file in enumerate(files):
+        path = os.path.join(directory, file)
+        if os.path.isdir(path):
+            new_prefix = prefix + ("├── " if i < len(files) - 1 else "└── ")
+            print(f"{prefix}{('├──' if i < len(files) - 1 else '└──')} {file}/")
+            print_md5_for_directory(path, prefix + ("│   " if i < len(files) - 1 else "    "))
+        else:
+            md5_value = get_md5_for_file(path)
+            print(f"{prefix}{('├──' if i < len(files) - 1 else '└──')} {file}: {md5_value}")
+
 def main():
     """主函数"""
     print("选择MD5计算类型：")
     print("1. 文件")
     print("2. 字符串")
-    choice = input("请输入选项（1或2）: ")
+    print("3. 文件夹")
+    choice = input("请输入选项（1, 2或3）: ")
 
     if choice == '1':
         file_path = input("请输入文件路径: ").strip("\"")
@@ -33,6 +53,10 @@ def main():
         text = input("请输入字符串: ")
         md5_value = get_md5_for_string(text)
         print(f"'{text}'的MD5值是：{md5_value}")
+    elif choice == '3':
+        directory_path = input("请输入文件夹路径: ").strip("\"")
+        print(f"'{directory_path}'文件夹内的文件MD5值是：")
+        print_md5_for_directory(directory_path)
     else:
         print("无效的选项")
 
